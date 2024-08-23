d0d1d277e91225ddf74c38088c4977915cc9b83c
SHIBADRITA SAHA <152030215+sibadrita23@users.noreply.github.com>
2024-08-23 12:46:52 +0530

    Update data_loading.R

--- R_Scripts/data_loading.R
+++ R_Scripts/data_loading.R
@@ -5,3 +5,12 @@ install.packages("readr")
 
 # Load libraries
 library(readr)
+# Load the global temperature data
+temperature_data <- read_csv("../Data/global_temperature_data.csv")
+
+# Display first few rows of the data
+head(temperature_data)
+
+# Structure of the dataset
+str(temperature_data)
+
